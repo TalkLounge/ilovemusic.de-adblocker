eb0c087be2587583c0938f0a7b5c42cdceae2a0e
TalkLounge <talklounge@yahoo.de>
2021-12-27 00:46:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         ilovemusic.de disable ads
-// @version      1.0.1
+// @version      1.0.2
 // @description  Disables ads on ilovemusic.de
 // @author       TalkLounge (https://github.com/TalkLounge)
 // @namespace    https://github.com/TalkLounge/ilovemusic.de-disable-ads
 // @license      MIT
-// @match        https://www.ilovemusic.de*
+// @match        https://www.ilovemusic.de/*
 // @grant        none
 // ==/UserScript==
 
